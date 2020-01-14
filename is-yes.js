b6812c677d4b6b7563fd24095030d1028b9a0f5c
Cody Brown <cody6lefever@gmail.com>
2020-01-14 10:04:28 -0800

    working on testing

--- is-yes.js
+++ is-yes.js
@@ -1,7 +1,6 @@
-//redo this as your own
 export default isYes;
-
+//the isYes in an if else function and whats allowed to be ready
 function isYes(str) {
-  if(str.toLowerCase() === 'yes' || str.toLowerCase() === 'y') return true;
+  if(str.toLowerCase() === 'yes' || str.toLowerCase() === 'y' || str.toLowerCase() === 'yep' || str.toLowerCase() === 'uhhuh') return true;
   else return false;
 }
--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-
+import isYes from '../src/is-yes.js';
 const test = QUnit.test;
 
 test('time to test a function', function(assert) {
@@ -14,3 +14,51 @@ test('time to test a function', function(assert) {
     // Make assertions about what is expected valid result
     assert.equal(true, false);
 });
+
+
+test('yep is true', assert => {
+    const input = 'yep';
+    const expected = true;
+    const reslut = isYes(input);
+    assert.equal(result, expected)
+  }
+
+
+
+test('Yes is true', assert => {
+  // arrange
+  const input = 'Yes';
+  const expected = true;
+  // act
+  
+  const result = isYes(input);
+
+  // assert
+  assert.equal(result, expected);
+});
+
+test('No is false', assert => {
+  // arrange
+  const input = 'No';
+  const expected = false;
+  // act
+  
+  const result = isYes(input);
+
+  // assert
+  assert.equal(result, expected);
+});
+
+test('Y is true', assert => {
+  // arrange
+  const input = 'Y';
+  const expected = true;
+  // act
+  
+  const result = isYes(input);
+
+  // assert
+  assert.equal(result, expected);
+});
+
+
